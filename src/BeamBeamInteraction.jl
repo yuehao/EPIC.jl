3db1b4e841c2efdb954880ea03e2085599fbb75a
Yue Hao <yuehao@msn.com>
2021-08-13 15:28:08 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.17

---
+++ src/BeamBeamInteraction.jl
@@ -0,0 +1,5 @@
+module BeamBeamInteraction
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BeamBeamInteraction
+using Test
+
+@testset "BeamBeamInteraction.jl" begin
+    # Write your tests here.
+end
