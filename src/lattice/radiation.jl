9683358e79c4125eef3488bcb850ffc141aafa14
Yue Hao <haoyue@msu.edu>
2024-05-01 11:38:23 -0400

    fix radiation for longitudinal motion

--- src/lattice/radiation.jl
+++ src/lattice/radiation.jl
@@ -7,26 +7,27 @@ struct OneTurnRadiation <: AbstractRadiation
         damping_turns=SVector{3,Float64}(damping_turnx, damping_turny, damping_turnz)
         damping_coefs=1.0./damping_turns
         damping_exp=exp.((-1.0).*damping_coefs)
+        damping_exp[3] = damping_exp[3] * damping_exp[3] # Special treatment for dp, no damping in z
         new(damping_turns, damping_coefs, damping_exp)
     end
 end
 
 function track!(ps6dcoor::AbstractVector{ps6d{T}}, otr::OneTurnRadiation, eqbs::AbstractVector) where T
     sqrt_exp=sqrt.(1 .- otr.damping_exp .* otr.damping_exp)
-    
+       
     @inbounds Threads.@threads for i in eachindex(ps6dcoor.x)
         ps6dcoor.x[i] *= otr.damping_exp[1]
         ps6dcoor.px[i] *= otr.damping_exp[1]
         ps6dcoor.y[i] *= otr.damping_exp[2]
         ps6dcoor.py[i] *= otr.damping_exp[2]
-        ps6dcoor.z[i] *= otr.damping_exp[3]
+        #ps6dcoor.z[i] *= otr.damping_exp[3]
         ps6dcoor.dp[i] *= otr.damping_exp[3]
 
         ps6dcoor.x[i] += eqbs[1]*sqrt_exp[1] * randn()
         ps6dcoor.px[i] += eqbs[2]*sqrt_exp[1] * randn()
         ps6dcoor.y[i] += eqbs[3]*sqrt_exp[2] * randn()
         ps6dcoor.py[i] += eqbs[4]*sqrt_exp[2] * randn()
-        ps6dcoor.z[i] += eqbs[5]*sqrt_exp[3] * randn()
+        #ps6dcoor.z[i] += eqbs[5]*sqrt_exp[3] * randn()
         ps6dcoor.dp[i] += eqbs[6]*sqrt_exp[3] * randn()
     end
     
